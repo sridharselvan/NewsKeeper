feeb19b44b9cc55f672a57fe06ec632db98252b2
Sridhar <sridharselvan@protonmail.com>
2020-02-16 01:13:04 +0530

    added create acc page and its state

--- static/js/app.js
+++ static/js/app.js
@@ -5,31 +5,34 @@ var app = angular.module("notesKeeper", ['ui.router']);
 app.config(function($stateProvider, $urlRouterProvider, $httpProvider) { 
   
     // creating routes or states
-    var loginState = {
+    var loginState, signupState;
+    loginState = {
         name: 'login',
         url: '/',
         templateUrl: '/login-page.html',
         controller: 'LoginCtrl'
     };
+    createAccountState = {
+        name: 'createAccount',
+        url: '/createAccount',
+        templateUrl: '/create-account-page.html'
+    };
     $stateProvider.state(loginState);
-    /*$stateProvider 
-        .state('Login', { 
-            url : '/', 
-            templateUrl: "login-page.html", 
-            controller : "LoginCtrl"
-        }) 
-        .state('Signup', { 
-            url : '/signup', 
-            templateUrl : "<h1>Signup Page</h1>", 
-            controller : "SignupCtrl"
-        });*/ 
+    $stateProvider.state(createAccountState);
   
-    // Redirect to home page if url does not  
-    // matches any of the three mentioned above 
+    // Redirect to home page if url does not matches any of the three mentioned above 
     $urlRouterProvider.otherwise("/"); 
 }); 
-  
-// create empty controllers for the states as we are 
-// not doing anything but just displaying message 
-app.controller('MainCtrl', function($scope, $http, $state, $stateParams) {}); 
-app.controller('LoginCtrl', function($scope, $http, $state, $stateParams) {});
+
+app.controller('LoginCtrl', function($scope, $http, $state, $stateParams) {
+    $scope.createUser = function(){
+        $state.transitionTo('createAccount');
+    };
+});
+app.controller('createAccountCtrl', function($scope, $http, $state) {
+    $scope.addUser = function() {
+        $http.post('/add_user', {'formObj' : $scope.userDetails}).then(function(response) {
+            $state.transitionTo('login');
+        });
+    };
+});
