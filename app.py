8e312421b92431418ece11b0f571bdcab02e3773
Sridhar <sridharselvan@protonmail.com>
2020-02-27 00:56:37 +0530

    sperate route file

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 import os
 from flask import Flask, send_from_directory, request
+from routes import *
 
 app = Flask(__name__, static_url_path='/') 
 app.static_folder="static"
-app_route = app.route
+# app_route = app.route
+app.register_blueprint(routes)
 
 @app.route('/', defaults={'path': 'index'}, methods=['GET', 'POST'])
 @app.route('/<path:path>.html', methods=['GET', 'POST'])
 def views(path):
     html_path = 'htmls/{}.html'.format(path)
     return app.send_static_file(html_path)
-
-@app_route('/add_user', methods=['POST'])
-def add_user():
-	form_data = request.get_json()
-	return {'msg': 'ok'}
 
 if __name__ == '__main__':
     app.run(port=4770, debug=True)
